4ad12a688d977cd8c7cf9da88ca6ee8e0e0792bd
Eda Z Uyanık <72568009+EdaZeynepU@users.noreply.github.com>
2024-05-16 01:28:32 +0300

    techlonogies handled

--- DataTransformation/xls_to_csv.py
+++ DataTransformation/xls_to_csv.py
@@ -4,6 +4,7 @@ dataframe1 = pd.read_excel('2024-yazilim-sektoru-maaslari.xlsx')
 
 dataframe1.loc[dataframe1["gender"] == "Erkek", "gender"] = 1
 dataframe1.loc[dataframe1["gender"] == "Kadın", "gender"] = 0
+dataframe1.loc[dataframe1["gender"] == "Belirtmek İstemiyorum", "gender"] = 2
 
 dataframe1.loc[dataframe1["currency"] != "₺ - Türk Lirası", "currency"] = 0
 dataframe1.loc[dataframe1["currency"] == "₺ - Türk Lirası", "currency"] = 1
@@ -23,9 +24,10 @@ dataframe1.loc[dataframe1["experience"] == "15 Yıl ve üzeri", "experience"] = 
 
 
 company_size_mapping = {"1 - 5 Kişi": 1, "6 - 10 Kişi": 2, "11 - 20 Kişi": 3,"21 - 50 Kişi": 4,"51 - 100 Kişi": 5,"101 - 249 Kişi": 6,"250+": 7}
+work_type_mapping = {"Ofis": 1, "Hibrit (Ofis + Remote)": 2, "Remote": 3,"Şu an remote ama hibrite döneceğiz.": 4,"Şu an hibrit ama ofise döneceğiz.": 5}
 
 dataframe1["company_size"] = dataframe1["company_size"].map(company_size_mapping)
-
+dataframe1["work_type"] = dataframe1["work_type"].map(work_type_mapping)
 
 # first we want to detect different countries
 dataframe1.loc[dataframe1['city'].str.contains(r'^\* ', na=False), 'city'] = 3
@@ -33,3 +35,43 @@ dataframe1.loc[dataframe1['city'].str.contains(r'^\* ', na=False), 'city'] = 3
 dataframe1.loc[(dataframe1["city"] != "İstanbul") & (dataframe1["city"] != 3), "city"] = 1
 # then find and assign 1 to istanbuls
 dataframe1.loc[dataframe1["city"] == "İstanbul", "city"] = 2
+
+
+# messss -----------------------------------------------------------------------
+# tech mapping starts ---------------
+techs = []
+tech_stack_mapping = {}
+def addToList(tech_stack):
+    global techs
+    try:
+        tech_list = tech_stack.split(';')
+        techs.extend(tech_list)
+    except AttributeError:
+        return ()
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return ()
+    
+dataframe1['tech_stack_tuple'] = dataframe1['tech_stack'].apply(addToList)
+counter = 0
+
+# tech mapping ends -----------
+
+for tech in techs:
+    tech_stack_mapping[tech] = techs.count(tech)
+
+techSearchList=[]
+for i in sorted(tech_stack_mapping.items() ,reverse=True, key=lambda x:x[1])[:16]:
+    techSearchList.append(i[0])
+# print(sorted(tech_stack_mapping.items(), key=lambda x:x[1]))
+
+print(techSearchList)
+
+
+for techDF in techSearchList:
+    dataframe1.loc[dataframe1["tech_stack"].str.contains(r''+techDF+'', na=False), techDF] = True
+    dataframe1.loc[dataframe1["tech_stack"].str.contains(r''+techDF+'', na=False) == False, techDF] = False
+
+
+print(dataframe1)
+# mess ends---------------------------------------------------------------------
