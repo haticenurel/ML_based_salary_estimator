42179147967fd610a5cfd4be76207ddd403512ad
Eda Z Uyanık <72568009+EdaZeynepU@users.noreply.github.com>
2024-05-27 21:31:36 +0300

    Update xls_to_csv.py

--- DataTransformation/xls_to_csv.py
+++ DataTransformation/xls_to_csv.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import xlsxwriter
 from pathlib import Path
-# path=Path("-/DataTransformation/2024-yazilim-sektoru-maaslari.xlsx")
-path=Path("2024_Yazılım_Sektörü_Maaş.xlsx")
+path=Path("2024-yazilim-sektoru-maaslari.xlsx")
+# path=Path("2024_Yazılım_Sektörü_Maaş.xlsx")
 
 dataframe1 = pd.read_excel(path)
 dataframe1 = pd.read_excel(path)
@@ -45,42 +45,43 @@ dataframe1.loc[dataframe1["company"] == "Kurumsal", "company"] = 1
 
 
 
-# messss -----------------------------------------------------------------------
-# tech mapping starts ---------------
-techs = []
-tech_stack_mapping = {}
-def addToList(tech_stack):
-    global techs
-    try:
-        tech_list = tech_stack.split('\n')
-        techs.extend(tech_list)
-    except AttributeError:
-        return ()
-    except Exception as e:
-        print(f"Error occurred: {e}")
-        return ()
+# # messss DATA 2024_Yazılım_Sektörü_Maaş.xlsx -----------------------------------------------------------------------
+# # tech mapping starts ---------------
+# techs = []
+# tech_stack_mapping = {}
+# def addToList(tech_stack):
+#     global techs
+#     try:
+#         tech_list = tech_stack.split('\n')
+#         techs.extend(tech_list)
+#     except AttributeError:
+#         return ()
+#     except Exception as e:
+#         print(f"Error occurred: {e}")
+#         return ()
     
-dataframe1['Ağırlıklı olarak hangi teknolojileri / dilleri kullanıyorsunuz?'].apply(addToList)
-counter = 0
+# dataframe1['Ağırlıklı olarak hangi teknolojileri / dilleri kullanıyorsunuz?'].apply(addToList)
+# counter = 0
 
-# tech mapping ends -----------
+# # tech mapping ends -----------
 
-for tech in techs:
-    tech_stack_mapping[tech] = techs.count(tech)
-# print(tech_stack_mapping)
-techSearchList=[]
-for i in sorted(tech_stack_mapping.items() ,reverse=True, key=lambda x:x[1])[:24]:
-    techSearchList.append(i[0])
-    print(i[1])
+# for tech in techs:
+#     tech_stack_mapping[tech] = techs.count(tech)
+# # print(tech_stack_mapping)
+# techSearchList=[]
+# for i in sorted(tech_stack_mapping.items() ,reverse=True, key=lambda x:x[1])[:24]:
+#     techSearchList.append(i[0])
+#     print(i[1])
 
 
 
-for techDF in techSearchList:
-    dataframe1.loc[dataframe1["Ağırlıklı olarak hangi teknolojileri / dilleri kullanıyorsunuz?"].str.contains(r''+techDF+'', na=False), techDF] = int(1)
-    dataframe1.loc[dataframe1["Ağırlıklı olarak hangi teknolojileri / dilleri kullanıyorsunuz?"].str.contains(r''+techDF+'', na=False) == False, techDF] = int(0)
+# for techDF in techSearchList:
+#     dataframe1.loc[dataframe1["Ağırlıklı olarak hangi teknolojileri / dilleri kullanıyorsunuz?"].str.contains(r''+techDF+'', na=False), techDF] = int(1)
+#     dataframe1.loc[dataframe1["Ağırlıklı olarak hangi teknolojileri / dilleri kullanıyorsunuz?"].str.contains(r''+techDF+'', na=False) == False, techDF] = int(0)
 
-# messss ends -------------------------------------------------------------------
-# messss -----------------------------------------------------------------------
+# # messss ends -------------------------------------------------------------------
+
+# messss DATA 2024-yazilim-sektoru-maaslari.xlsx -----------------------------------------------------------------------
 # tech mapping starts ---------------
 techs = []
 tech_stack_mapping = {}
@@ -104,7 +105,7 @@ for tech in techs:
     tech_stack_mapping[tech] = techs.count(tech)
 # print(tech_stack_mapping)
 techSearchList=[]
-for i in sorted(tech_stack_mapping.items() ,reverse=True, key=lambda x:x[1])[:17]:
+for i in sorted(tech_stack_mapping.items() ,reverse=True, key=lambda x:x[1])[:16]:
     techSearchList.append(i[0])
     print(i[1])
 
@@ -116,14 +117,14 @@ for techDF in techSearchList:
 
 # messss ends -------------------------------------------------------------------
 
-# # dataframe1["Office"] = dataframe1["work_type"].apply(lambda work_type: 1 if work_type == "Ofis" or work_type == "Ofiste" else 0)
-# # dataframe1["Hybrid"] = dataframe1["work_type"].apply(lambda work_type: 1 if work_type == "Hibrit" or work_type == "Hybrid" or work_type == "Hibrit (Ofis + Remote)" or work_type == "Şu an hibrit ama ofise döneceğiz." else 0)
-# # dataframe1["Remote"] = dataframe1["work_type"].apply(lambda work_type: 1 if work_type == "Şu an remote ama hibrite döneceğiz." or work_type == "Remote" else 0)
-# # dataframe1.drop(['work_type'], axis=1,inplace=True)
+# dataframe1["Office"] = dataframe1["work_type"].apply(lambda work_type: 1 if work_type == "Ofis" or work_type == "Ofiste" else 0)
+# dataframe1["Hybrid"] = dataframe1["work_type"].apply(lambda work_type: 1 if work_type == "Hibrit" or work_type == "Hybrid" or work_type == "Hibrit (Ofis + Remote)" or work_type == "Şu an hibrit ama ofise döneceğiz." else 0)
+# dataframe1["Remote"] = dataframe1["work_type"].apply(lambda work_type: 1 if work_type == "Şu an remote ama hibrite döneceğiz." or work_type == "Remote" else 0)
+# dataframe1.drop(['work_type'], axis=1,inplace=True)
 
-# # to new file
-# file_path = 'outputOther1.xlsx'
-# dataframe1.to_excel(file_path, index=False)
+# to new file
+file_path = 'outputOther1.xlsx'
+dataframe1.to_excel(file_path, index=False)
 
 
 
